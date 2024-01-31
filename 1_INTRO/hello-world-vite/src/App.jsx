243a8b101d5c25496f20b821bdefc3280fb0466a
Bruno <bru-a@hotmail.com>
2024-01-31 19:37:56 -0300

    first commit

---
+++ 1_INTRO/hello-world-vite/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <div className='centerDiv'>
+        <h2>Are you ok?</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
